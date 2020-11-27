18294b0fc3a5d526e0f1f8585cdeb802ac253e03
grootsadmin <54016945+grootsadmin@users.noreply.github.com>
2020-11-27 16:57:11 +0530

    Update addplugin.sh

--- v5/bin/addplugin.sh
+++ v5/bin/addplugin.sh
@@ -48,19 +48,20 @@ while read line; do echo "[`date +"%Y-%m-%dT%H:%M:%S,%N" | rev | cut -c 7- | rev
 
 if [ "${1}" = "--help" -o "${#}" != "2" ];
 then
-echo -e "Plugin list: sms, appsensors, aws, backup, dns, docker, elk, expiry, hardware, lamp, mithi, os, website, jvm, node, jenkins
+echo -e "
+        Plugin list: sms, appsensors, aws, backup, dns, docker, elk, expiry, hardware, lamp, mithi, os, website, jvm, node, jenkins
 
-OPTION                 DESCRIPTION
------------------------------------------
---help                   help
--p [plugin]            plugin name
------------------------------------------
+        OPTION                 DESCRIPTION
+        -----------------------------------------
+        --help                   help
+        -p [plugin]            plugin name
+        -----------------------------------------
 
-Usage: sh $SCRIPTNAME  -p [plugin name]
-Ex: sh addplugin.sh -p aws
-";
-exit 3;
+        Usage: sh $SCRIPTNAME  -p [plugin name]
+        Ex: sh addplugin.sh -p aws";
+    exit 3;
 fi
+
 
 #######################################################
 # Get user-given variables
@@ -68,32 +69,32 @@ fi
 
 while getopts "p:" Input;
 do
-	case ${Input} in
-	p) PLUGINNAME=$OPTARG;;
-	*) echo "Usage: $SCRIPTNAME -p [plugin name]"
-	exit 3;
-	;;
-	esac
+        case ${Input} in
+        p) PLUGINNAME=$OPTARG;;
+        *) echo "Usage: $SCRIPTNAME -p [plugin name]"
+        exit 3;
+        ;;
+        esac
 done
 
-# Get OS 
+# Get OS
 #######################################################
 
 OSNAME=$(cat /etc/*release | grep "PRETTY_NAME" | sed 's/PRETTY_NAME=//g' | sed 's/["]//g' | awk '{print $1}')
-echo "#######################################################" | log 
-echo "Verifying if Subversion is present or not.." | log 
+echo "#######################################################" | log
+echo "Verifying if Subversion is present or not.." | log
 
 if [ $OSNAME = "CentOS" ]; then
 
     svn --version > /dev/null 2>&1
 
     if [ $? -eq 0 ]; then
-    	echo "#######################################################" | log 
-	echo "Svn is installed!" | log 
+        echo "#######################################################" | log
+        echo "Svn is installed!" | log
     else
-        echo "Svn is not installed! Exiting now.." | log 
+        echo "Svn is not installed! Exiting now.." | log
         echo "#######################################################"
-        echo "To install svn: yum install subversion -y " | log 
+        echo "To install svn: yum install subversion -y " | log
         exit 3;
     fi
 fi
@@ -104,12 +105,12 @@ if [ $OSNAME = "Ubuntu" ]; then
     svn --version > /dev/null 2>&1
 
     if [ $? -eq 0 ]; then
-        echo "########################################################" | log 
-	echo "Svn is installed!" | log 
+        echo "########################################################" | log
+        echo "Svn is installed!" | log
     else
-        echo "Svn is not installed! Exiting now.." | log 
-	echo "########################################################" | log 
-        echo "To install svn: apt install subversion  " | log 
+        echo "Svn is not installed! Exiting now.." | log
+        echo "########################################################" | log
+        echo "To install svn: apt install subversion  " | log
         exit 3;
     fi
 fi
@@ -118,12 +119,12 @@ fi
 # collecting git Username and Password
 #######################################################
 
-echo "#######################################################" | log 
-echo "Enter git username:" | log 
+echo "#######################################################" | log
+echo "Enter git username:" | log
 read USERNAME
 
-echo "#######################################################" | log 
-echo "Enter git Password:" | log 
+echo "#######################################################" | log
+echo "Enter git Password:" | log
 read -s PASSWORD
 
 # Destination path
@@ -138,94 +139,94 @@ SVNCMD="--non-interactive --no-auth-cache --username $USERNAME --password "$PASS
 GITPATH="svn checkout https://github.com/grootsadmin/gmetrics-plugins/trunk/os/linux"
 
 if [ "$PLUGINNAME" = "sms" ] || [ "$PLUGINNAME" = "Sms" ] || [ "$PLUGINNAME" = "SMS" ] ; then
-    echo "Plugin "sms" selected to add in "$DEST" " | log 
+    echo "Plugin "sms" selected to add in "$DEST" " | log
     $GITPATH/sms $SVNCMD
-	rm -rf $DEST/.svn
-    echo "$PLUGINNAME plugin successfully added. " | log 
+        rm -rf $DEST/.svn
+    echo "$PLUGINNAME plugin successfully added. " | log
 
 
 elif [ "$PLUGINNAME" = "aws" ] || [ "$PLUGINNAME" = "Aws" ] || [ "$PLUGINNAME" = "AWS" ] ; then
-    echo "Plugin "aws" selected to add in "$DEST" " | log 
+    echo "Plugin "aws" selected to add in "$DEST" " | log
     $GITPATH/aws $SVNCMD
-	rm -rf $DEST/.svn
-    echo "$PLUGINNAME plugin successfully added. " | log 
+        rm -rf $DEST/.svn
+    echo "$PLUGINNAME plugin successfully added. " | log
 
 
 elif [ "$PLUGINNAME" = "appsensors" ] || [ "$PLUGINNAME" = "Appsensors" ] || [ "$PLUGINNAME" = "APPSENSORS" ]; then
-    echo "Plugin "appsensors" selected to add in "$DEST" " | log 
+    echo "Plugin "appsensors" selected to add in "$DEST" " | log
     $GITPATH/appsensors $SVNCMD
-	rm -rf $DEST/.svn
-    echo "$PLUGINNAME plugin successfully added. " | log 
+        rm -rf $DEST/.svn
+    echo "$PLUGINNAME plugin successfully added. " | log
 
 
 elif [ "$PLUGINNAME" = "expiry" ] || [ "$PLUGINNAME" = "Expiry" ] || [ "$PLUGINNAME" = "EXPIRY" ]; then
-    echo "Plugin "expiry" selected to add in "$DEST" " | log 
+    echo "Plugin "expiry" selected to add in "$DEST" " | log
     $GITPATH/expiry $SVNCMD
-	rm -rf $DEST/.svn
-    echo "$PLUGINNAME plugin successfully added. " | log 
+        rm -rf $DEST/.svn
+    echo "$PLUGINNAME plugin successfully added. " | log
 
 
 elif [ "$PLUGINNAME" = "backup" ] || [ "$PLUGINNAME" = "Backup" ] || [ "$PLUGINNAME" = "BACKUP" ]; then
-    echo "Plugin "backup" selected to add in "$DEST" " | log 
+    echo "Plugin "backup" selected to add in "$DEST" " | log
     $GITPATH/backup $SVNCMD
-	rm -rf $DEST/.svn
-    echo "$PLUGINNAME plugin successfully added. " | log 
+        rm -rf $DEST/.svn
+    echo "$PLUGINNAME plugin successfully added. " | log
 
 
 elif [ "$PLUGINNAME" = "dns" ] || [ "$PLUGINNAME" = "DNS" ] || [ "$PLUGINNAME" = "dns" ]; then
-    echo "Plugin "dns" selected to add in "$DEST" " | log 
+    echo "Plugin "dns" selected to add in "$DEST" " | log
     $GITPATH/dns $SVNCMD
-	rm -rf $DEST/.svn
-    echo "$PLUGINNAME plugin successfully added. " | log 
+        rm -rf $DEST/.svn
+    echo "$PLUGINNAME plugin successfully added. " | log
 
 
 elif [ "$PLUGINNAME" = "docker" ] || [ "$PLUGINNAME" = "Docker" ] || [ "$PLUGINNAME" = "DOCKER" ]; then
-    echo "Plugin "docker" selected to add in "$DEST" " | log 
+    echo "Plugin "docker" selected to add in "$DEST" " | log
     $GITPATH/docker $SVNCMD
-	rm -rf $DEST/.svn
-    echo "$PLUGINNAME successfully added. " | log 
+        rm -rf $DEST/.svn
+    echo "$PLUGINNAME successfully added. " | log
 
 
 elif [ "$PLUGINNAME" = "hardware" ] || [ "$PLUGINNAME" = "Hardware" ] || [ "$PLUGINNAME" = "HARDWARE" ]; then
-    echo "Plugin "hardware" selected to add in "$DEST" " | log 
+    echo "Plugin "hardware" selected to add in "$DEST" " | log
     $GITPATH/hardware $SVNCMD
-	rm -rf $DEST/.svn
-    echo "$PLUGINNAME plugin successfully added. " | log 
+        rm -rf $DEST/.svn
+    echo "$PLUGINNAME plugin successfully added. " | log
 
 
 elif [ "$PLUGINNAME" = "lamp" ] || [ "$PLUGINNAME" = "Lamp" ] || [ "$PLUGINNAME" = "LAMP" ]; then
-    echo "Plugin "lamp" selected to add in "$DEST" " | log 
+    echo "Plugin "lamp" selected to add in "$DEST" " | log
     $GITPATH/lamp $SVNCMD
-	rm -rf $DEST/.svn
-    echo "$PLUGINNAME plugin successfully added. " | log 
+        rm -rf $DEST/.svn
+    echo "$PLUGINNAME plugin successfully added. " | log
 
 
 elif [ "$PLUGINNAME" = "mithi" ] || [ "$PLUGINNAME" = "Mithi" ] || [ "$PLUGINNAME" = "MITHI" ]; then
-    echo "Plugin "mithi" selected to add in "$DEST" " | log 
+    echo "Plugin "mithi" selected to add in "$DEST" " | log
     $GITPATH/mithi $SVNCMD
-	rm -rf $DEST/.svn
-    echo "$PLUGINNAME plugin successfully added. " | log 
+        rm -rf $DEST/.svn
+    echo "$PLUGINNAME plugin successfully added. " | log
 
 
 elif [ "$PLUGINNAME" = "os" ] || [ "$PLUGINNAME" = "Os" ] || [ "$PLUGINNAME" = "OS" ]; then
-    echo "Plugin "os" selected to add in "$DEST" " | log 
+    echo "Plugin "os" selected to add in "$DEST" " | log
     $GITPATH/os $SVNCMD
-	rm -rf $DEST/.svn
-    echo "$PLUGINNAME plugin successfully added. " | log 
+        rm -rf $DEST/.svn
+    echo "$PLUGINNAME plugin successfully added. " | log
 
 
 elif [ "$PLUGINNAME" = "jvm" ] || [ "$PLUGINNAME" = "Jvm" ] || [ "$PLUGINNAME" = "JVM" ]; then
-    echo "Plugin "jvm" selected to add in "$DEST" " | log 
+    echo "Plugin "jvm" selected to add in "$DEST" " | log
     $GITPATH/jvm $SVNCMD
-	rm -rf $DEST/.svn
-    echo "$PLUGINNAME plugin successfully added. " | log 
+        rm -rf $DEST/.svn
+    echo "$PLUGINNAME plugin successfully added. " | log
 
 
 elif [ "$PLUGINNAME" = "website" ] || [ "$PLUGINNAME" = "Website" ] || [ "$PLUGINNAME" = "WEBSITE" ]; then
-    echo "Plugin "website" selected to add in "$DEST" " | log 
+    echo "Plugin "website" selected to add in "$DEST" " | log
     $GITPATH/website $SVNCMD
-	rm -rf $DEST/.svn
-    echo "$PLUGINNAME plugin successfully added. " | log 
+        rm -rf $DEST/.svn
+    echo "$PLUGINNAME plugin successfully added. " | log
 
 elif [ "$PLUGINNAME" = "node" ] || [ "$PLUGINNAME" = "Node" ] || [ "$PLUGINNAME" = "NODE" ]; then
     echo "Plugin "node" selected to add in "$DEST" " | log
@@ -253,4 +254,3 @@ fi
 
 # End Main logic.
 #######################################################
-
