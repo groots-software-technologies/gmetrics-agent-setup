5e1cbe13593564dc6146eb872b9dbf8184a78a63
root <harish@groots.in>
2020-12-01 14:42:55 +0530

    update

--- v5/bin/addplugin.sh
+++ v5/bin/addplugin.sh
@@ -17,7 +17,8 @@
 
 #Set script name
 #######################################################
-SCRIPTNAME=$(basename $0)
+
+SCRIPTNAME="addplugin.sh"
 
 # Import Hostname
 #######################################################
@@ -49,17 +50,16 @@ while read line; do echo "[`date +"%Y-%m-%dT%H:%M:%S,%N" | rev | cut -c 7- | rev
 if [ "${1}" = "--help" -o "${#}" != "2" ];
 then
 echo -e "
-        Plugin list: sms, appsensors, aws, backup, dns, docker, elk, expiry, hardware, lamp, mithi, os, website, jvm, node, jenkins
+Plugin list: sms, appsensors, aws, backup, dns, docker, elk, expiry, hardware, lamp, mithi, os, website, jvm, node, jenkins
 
-        OPTION                 DESCRIPTION
-        -----------------------------------------
-        --help                   help
-        -p [plugin]            plugin name
-        -----------------------------------------
-
-        Usage: sh $SCRIPTNAME  -p [plugin name]
-        Ex: sh addplugin.sh -p aws";
-    exit 3;
+OPTION                 DESCRIPTION
+-----------------------------------------
+--help                   help
+-p [plugin]            plugin name
+-----------------------------------------
+Usage: sh $SCRIPTNAME  -p [plugin name]
+Ex: sh addplugin.sh -p aws";
+exit 3;
 fi
 
 
@@ -84,40 +84,15 @@ OSNAME=$(cat /etc/*release | grep "PRETTY_NAME" | sed 's/PRETTY_NAME=//g' | sed 
 echo "#######################################################" | log
 echo "Verifying if Subversion is present or not.." | log
 
-if [ $OSNAME = "CentOS" ]; then
+# type command is checking whether svn command present or not.
+#######################################################
 
-    svn --version > /dev/null 2>&1
-
-    if [ $? -eq 0 ]; then
-        echo "#######################################################" | log
-        echo "Svn is installed!" | log
-    else
-        echo "Svn is not installed! Exiting now.." | log
-        echo "#######################################################"
-        echo "To install svn: yum install subversion -y " | log
-        exit 3;
-    fi
-fi
-
-
-if [ $OSNAME = "Ubuntu" ]; then
-
-    svn --version > /dev/null 2>&1
-
-    if [ $? -eq 0 ]; then
-        echo "########################################################" | log
-        echo "Svn is installed!" | log
-    else
-        echo "Svn is not installed! Exiting now.." | log
-        echo "########################################################" | log
-        echo "To install svn: apt install subversion  " | log
-        exit 3;
-    fi
-fi
-
+type svn >/dev/null 2>&1 || { echo >&2 "This plugin require "subversion" package, but it's not installed. Aborting."; exit 1; }
 
 # collecting git Username and Password
 #######################################################
+
+user_input () {
 
 echo "#######################################################" | log
 echo "Enter git username:" | log
@@ -127,134 +102,165 @@ echo "#######################################################" | log
 echo "Enter git Password:" | log
 read -s PASSWORD
 
+}
+
+quit () {
+
+echo "Wrong creds"
+exit 1;
+}
+
 # Destination path
 #######################################################
-
 DEST="/groots/metrics/libexec/"
 
 # Svn command to Download plugin folder from git to /groots/metrics/libexec
 #######################################################
+
+copy_plugin () {
 
 SVNCMD="--non-interactive --no-auth-cache --username $USERNAME --password "$PASSWORD" $DEST"
 GITPATH="svn checkout https://github.com/grootsadmin/gmetrics-plugins/trunk/os/linux"
 
 if [ "$PLUGINNAME" = "sms" ] || [ "$PLUGINNAME" = "Sms" ] || [ "$PLUGINNAME" = "SMS" ] ; then
     echo "Plugin "sms" selected to add in "$DEST" " | log
-    $GITPATH/sms $SVNCMD
+    $GITPATH/sms $SVNCMD  >/dev/null || { echo "Wrong git credentials, Exiting now..."; exit 1; }
         rm -rf $DEST/.svn
     echo "$PLUGINNAME plugin successfully added. " | log
 
 
 elif [ "$PLUGINNAME" = "aws" ] || [ "$PLUGINNAME" = "Aws" ] || [ "$PLUGINNAME" = "AWS" ] ; then
     echo "Plugin "aws" selected to add in "$DEST" " | log
-    $GITPATH/aws $SVNCMD
+    $GITPATH/aws $SVNCMD >/dev/null || { echo "Wrong git credentials, Exiting now..."; exit 1; }
         rm -rf $DEST/.svn
     echo "$PLUGINNAME plugin successfully added. " | log
 
 
 elif [ "$PLUGINNAME" = "appsensors" ] || [ "$PLUGINNAME" = "Appsensors" ] || [ "$PLUGINNAME" = "APPSENSORS" ]; then
     echo "Plugin "appsensors" selected to add in "$DEST" " | log
-    $GITPATH/appsensors $SVNCMD
+    $GITPATH/appsensors $SVNCMD >/dev/null || { echo "Wrong git credentials, Exiting now..."; exit 1; }
         rm -rf $DEST/.svn
     echo "$PLUGINNAME plugin successfully added. " | log
 
 
 elif [ "$PLUGINNAME" = "expiry" ] || [ "$PLUGINNAME" = "Expiry" ] || [ "$PLUGINNAME" = "EXPIRY" ]; then
     echo "Plugin "expiry" selected to add in "$DEST" " | log
-    $GITPATH/expiry $SVNCMD
+    $GITPATH/expiry $SVNCMD >/dev/null || { echo "Wrong git credentials, Exiting now..."; exit 1; }
         rm -rf $DEST/.svn
     echo "$PLUGINNAME plugin successfully added. " | log
 
 
 elif [ "$PLUGINNAME" = "backup" ] || [ "$PLUGINNAME" = "Backup" ] || [ "$PLUGINNAME" = "BACKUP" ]; then
     echo "Plugin "backup" selected to add in "$DEST" " | log
-    $GITPATH/backup $SVNCMD
+    $GITPATH/backup $SVNCMD >/dev/null || { echo "Wrong git credentials, Exiting now..."; exit 1; }
         rm -rf $DEST/.svn
     echo "$PLUGINNAME plugin successfully added. " | log
 
 
 elif [ "$PLUGINNAME" = "dns" ] || [ "$PLUGINNAME" = "DNS" ] || [ "$PLUGINNAME" = "dns" ]; then
     echo "Plugin "dns" selected to add in "$DEST" " | log
-    $GITPATH/dns $SVNCMD
+    $GITPATH/dns $SVNCMD >/dev/null || { echo "Wrong git credentials, Exiting now..."; exit 1; }
         rm -rf $DEST/.svn
     echo "$PLUGINNAME plugin successfully added. " | log
 
 
 elif [ "$PLUGINNAME" = "docker" ] || [ "$PLUGINNAME" = "Docker" ] || [ "$PLUGINNAME" = "DOCKER" ]; then
     echo "Plugin "docker" selected to add in "$DEST" " | log
-    $GITPATH/docker $SVNCMD
+    $GITPATH/docker $SVNCMD >/dev/null || { echo "Wrong git credentials, Exiting now..."; exit 1; }
         rm -rf $DEST/.svn
     echo "$PLUGINNAME successfully added. " | log
 
 
 elif [ "$PLUGINNAME" = "hardware" ] || [ "$PLUGINNAME" = "Hardware" ] || [ "$PLUGINNAME" = "HARDWARE" ]; then
     echo "Plugin "hardware" selected to add in "$DEST" " | log
-    $GITPATH/hardware $SVNCMD
+    $GITPATH/hardware $SVNCMD >/dev/null || { echo "Wrong git credentials, Exiting now..."; exit 1; }
         rm -rf $DEST/.svn
     echo "$PLUGINNAME plugin successfully added. " | log
 
 
 elif [ "$PLUGINNAME" = "lamp" ] || [ "$PLUGINNAME" = "Lamp" ] || [ "$PLUGINNAME" = "LAMP" ]; then
     echo "Plugin "lamp" selected to add in "$DEST" " | log
-    $GITPATH/lamp $SVNCMD
+    $GITPATH/lamp $SVNCMD >/dev/null || { echo "Wrong git credentials, Exiting now..."; exit 1; }
         rm -rf $DEST/.svn
     echo "$PLUGINNAME plugin successfully added. " | log
 
 
 elif [ "$PLUGINNAME" = "mithi" ] || [ "$PLUGINNAME" = "Mithi" ] || [ "$PLUGINNAME" = "MITHI" ]; then
     echo "Plugin "mithi" selected to add in "$DEST" " | log
-    $GITPATH/mithi $SVNCMD
+    $GITPATH/mithi $SVNCMD >/dev/null || { echo "Wrong git credentials, Exiting now..."; exit 1; }
         rm -rf $DEST/.svn
     echo "$PLUGINNAME plugin successfully added. " | log
 
 
 elif [ "$PLUGINNAME" = "os" ] || [ "$PLUGINNAME" = "Os" ] || [ "$PLUGINNAME" = "OS" ]; then
     echo "Plugin "os" selected to add in "$DEST" " | log
-    $GITPATH/os $SVNCMD
+    $GITPATH/os $SVNCMD >/dev/null || { echo "Wrong git credentials, Exiting now..."; exit 1; }
         rm -rf $DEST/.svn
     echo "$PLUGINNAME plugin successfully added. " | log
 
 
 elif [ "$PLUGINNAME" = "jvm" ] || [ "$PLUGINNAME" = "Jvm" ] || [ "$PLUGINNAME" = "JVM" ]; then
     echo "Plugin "jvm" selected to add in "$DEST" " | log
-    $GITPATH/jvm $SVNCMD
+    $GITPATH/jvm $SVNCMD >/dev/null || { echo "Wrong git credentials, Exiting now..."; exit 1; }
         rm -rf $DEST/.svn
     echo "$PLUGINNAME plugin successfully added. " | log
 
 
 elif [ "$PLUGINNAME" = "website" ] || [ "$PLUGINNAME" = "Website" ] || [ "$PLUGINNAME" = "WEBSITE" ]; then
     echo "Plugin "website" selected to add in "$DEST" " | log
-    $GITPATH/website $SVNCMD
+    $GITPATH/website $SVNCMD >/dev/null || { echo "Wrong git credentials, Exiting now..."; exit 1; }
         rm -rf $DEST/.svn
     echo "$PLUGINNAME plugin successfully added. " | log
 
 elif [ "$PLUGINNAME" = "node" ] || [ "$PLUGINNAME" = "Node" ] || [ "$PLUGINNAME" = "NODE" ]; then
     echo "Plugin "node" selected to add in "$DEST" " | log
-    $GITPATH/node $SVNCMD
+    $GITPATH/node $SVNCMD >/dev/null || { echo "Wrong git credentials, Exiting now..."; exit 1; }
     rm -rf $DEST/.svn
     echo "$PLUGINNAME plugin successfully added. " | log
 
 elif [ "$PLUGINNAME" = "jenkins" ] || [ "$PLUGINNAME" = "Jenkins" ] || [ "$PLUGINNAME" = "JENKINS" ]; then
     echo "Plugin "jenkins" selected to add in "$DEST" " | log
-    $GITPATH/jenkins $SVNCMD
+    $GITPATH/jenkins $SVNCMD >/dev/null || { echo "Wrong git credentials, Exiting now..."; exit 1; }
     rm -rf $DEST/.svn
     echo "$PLUGINNAME plugin successfully added. " | log
 
 elif [ "$PLUGINNAME" = "ELK" ] || [ "$PLUGINNAME" = "Elk" ] || [ "$PLUGINNAME" = "elk" ]; then
     echo "Plugin "elk" selected to add in "$DEST" " | log
-    $GITPATH/elk $SVNCMD
+    $GITPATH/elk $SVNCMD >/dev/null || { echo "Wrong git credentials, Exiting now..."; exit 1; }
     rm -rf $DEST/.svn
     echo "$PLUGINNAME plugin successfully added. " | log
 
 else
-    echo "#######################################################" | log 
-    echo "Invalid $PLUGINNAME. Enter valid plugin name." | log 
+    echo "#######################################################"
+    echo "Invalid $PLUGINNAME. Enter valid plugin name. Exiting now..!"
     exit 3;
 fi
 
-echo "#######################################################" | log 
-echo " To disable svn password store" | log 
-sudo echo 'store-plaintext-passwords = no' >> /root/.subversion/servers | log 
+}
+
+# To disable svn password cache storing
+#######################################################
+
+disable_svn_password () {
+
+echo "#######################################################" | log
+echo "Disabling svn password store" | log
+sudo echo 'store-plaintext-passwords = no' >> /root/.subversion/servers
+}
+
+
+# Function calling
+#######################################################
+
+while true
+do
+        user_input
+        copy_plugin
+        disable_svn_password
+
+break
+done
 
 # End Main logic.
 #######################################################
+
