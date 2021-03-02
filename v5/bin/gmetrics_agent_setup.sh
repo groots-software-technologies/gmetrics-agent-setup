7c4e28ce534aa9e0d9f60a0ad7f733c7d59418d8
grootsadmin <54016945+grootsadmin@users.noreply.github.com>
2021-03-02 12:27:35 +0530

    Update gmetrics_agent_setup.sh

--- v5/bin/gmetrics_agent_setup.sh
+++ v5/bin/gmetrics_agent_setup.sh
@@ -140,7 +140,7 @@ echo "#######################################################" | log
 echo "Downloading Agent builds under $PLUGINSDIR directory" | log
 URL="https://github.com/grootsadmin/gmetrics-agent-setup/trunk/v5/builds"
 svn checkout $URL $PLUGINSDIR > /dev/null &  PID=$!
-echo "THIS PROCESS TAKE SOME TIME, SO PLEASE BE PATIENCE WHILE GMETRICS AGENT INSTALLATION IS RUNNING..." | log
+echo "THIS PROCESS TAKES SOME TIME, SO PLEASE BE PATIENCE WHILE GMETRICS AGENT INSTALLATION IS RUNNING..." | log
 printf "["
 while kill -0 $PID 2> /dev/null; do
     printf  "....++...."
